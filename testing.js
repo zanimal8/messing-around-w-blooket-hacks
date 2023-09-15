4a6aa46f209d931e59e2f88aafe15c56c517189f
zanimal8 <48495357+zanimal8@users.noreply.github.com>
2023-09-15 14:31:48 -0400

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,136 @@
+(() => {
+  console.log("%c SCHOOLCHEATS.NET ", "background: #222; color: #8b5cf6;font-size:24px");
+  console.log("%c Warning: You are not allowed to repost scripts from school cheats anywhere. Violators will be banned from the platform. ", "background: #222; color: red;font-size:18px");
+  console.log("%c Identification: 50/97/48/50/58/99/55/101/58/56/56/98/58/53/48/48/48/58/54/53/57/48/58/53/98/57/51/58/50/51/100/99/58/56/102/55", "background: #222; color: #8b5cf6;font-size:12px");
+})();
+var _0x5b11e5 = function () {
+  var _0x340481 = true;
+  return function (_0x5c6fd1, _0x30ee6c) {
+    var _0x4dff54 = _0x340481 ? function () {
+      if (_0x30ee6c) {
+        var _0x55869a = _0x30ee6c.apply(_0x5c6fd1, arguments);
+        _0x30ee6c = null;
+        return _0x55869a;
+      }
+    } : function () {};
+    _0x340481 = false;
+    return _0x4dff54;
+  };
+}();
+var _0x165f02 = _0x5b11e5(this, function () {
+  return _0x165f02.toString().search("(((.+)+)+)+$").toString().constructor(_0x165f02).search("(((.+)+)+)+$");
+});
+_0x165f02();
+var _0x53d842 = function () {
+  var _0x2ef25f = true;
+  return function (_0x45d5a1, _0x43333e) {
+    var _0x322f84 = _0x2ef25f ? function () {
+      if (_0x43333e) {
+        var _0x24419d = _0x43333e.apply(_0x45d5a1, arguments);
+        _0x43333e = null;
+        return _0x24419d;
+      }
+    } : function () {};
+    _0x2ef25f = false;
+    return _0x322f84;
+  };
+}();
+var _0x2d5001 = _0x53d842(this, function () {
+  var _0x3c9353 = function () {
+    var _0x1bd183;
+    try {
+      _0x1bd183 = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x4d9c2a) {
+      _0x1bd183 = window;
+    }
+    return _0x1bd183;
+  };
+  var _0x1e1e06 = _0x3c9353();
+  var _0x58b4d1 = _0x1e1e06.console = _0x1e1e06.console || {};
+  var _0x84bdaa = ["log", 'warn', 'info', "error", "exception", "table", "trace"];
+  for (var _0x568270 = 0; _0x568270 < _0x84bdaa.length; _0x568270++) {
+    var _0x3cac0d = 0;
+    var _0x48a725 = _0x53d842.constructor.prototype.bind(_0x53d842);
+    var _0x158e0c = _0x84bdaa[_0x568270];
+    var _0x5a891c = _0x58b4d1[_0x158e0c] || _0x48a725;
+    _0x48a725.__proto__ = _0x53d842.bind(_0x53d842);
+    _0x48a725.toString = _0x5a891c.toString.bind(_0x5a891c);
+    _0x58b4d1[_0x158e0c] = _0x48a725;
+  }
+});
+_0x2d5001();
+(async () => {
+  try {
+    if (9999999999999999999 > 1655481426992) {
+      const _0x3adf6d = confirm("Script is outdated. Do you want to get the updated version?");
+      if (_0x3adf6d) {
+        return window.open("https://twitch.tv/");
+      }
+    } else {
+      (async () => {
+        var _0x11f9e1 = document.createElement("iframe");
+        document.body.appendChild(_0x11f9e1);
+        window.alert = _0x11f9e1.contentWindow.alert;
+        window.prompt = _0x11f9e1.contentWindow.prompt;
+        window.confirm = _0x11f9e1.contentWindow.confirm;
+        try {
+          var _0x1d90fa = document.querySelector("div[class*='arts__body']");
+          var _0x14d620 = Object.keys(_0x1d90fa).find(_0x576258 => _0x576258.includes("__reactEventHandlers"));
+          var _0x38f2d0 = _0x1d90fa[_0x14d620].children[1]._owner;
+          if (window.location.pathname == "/play/lobby") {
+            _0x38f2d0.memoizedState.lockedBlooks.length = 0;
+            _0x38f2d0.stateNode.forceUpdate();
+            alert("All blooks unlocked!");
+          } else {
+            alert("You have to use this hack in a live game www.blooket.com/play/lobby page.");
+          }
+          ;
+        } catch (_0x1a6dc5) {
+          if (confirm("An error occured, would you like to report this in the support discord server?")) {
+            window.open("https://glizzers.xyz/discord");
+          }
+          ;
+        }
+        ;
+      })();
+      function _0x562c4b() {
+        let _0x781f01 = document.createElement('div');
+        _0x781f01.style = "font-family: \"Nunito\", sans-serif; font-size: 14px; height: 65px; width: 175px; border: 4px solid rgb(15, 15, 15); background: rgb(240, 240, 240); position: absolute; top: 20x; left: 20px; border-radius: 10px; color: rgb(0, 0, 0); text-align: center;";
+        _0x781f01.innerHTML = "<p>Made by gliz <br> My <a style=\"color: #0000ff;\" href=\"https://twitter.com/glizuwu\" target=\"_blank\">twitter</a></p>";
+        document.body.appendChild(_0x781f01);
+        var _0x1e4055 = 0;
+        var _0x4319db = 0;
+        var _0x2ea376 = 0;
+        var _0x3395e3 = 0;
+        _0x781f01.onmousedown = (_0x476ba1 = window.event) => {
+          _0x476ba1.preventDefault();
+          _0x2ea376 = _0x476ba1.clientX;
+          _0x3395e3 = _0x476ba1.clientY;
+          document.onmouseup = () => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+          };
+          document.onmousemove = _0x52db60 => {
+            _0x52db60 = _0x52db60 || window.event;
+            _0x52db60.preventDefault();
+            _0x1e4055 = _0x2ea376 - _0x52db60.clientX;
+            _0x4319db = _0x3395e3 - _0x52db60.clientY;
+            _0x2ea376 = _0x52db60.clientX;
+            _0x3395e3 = _0x52db60.clientY;
+            let _0x384879 = _0x781f01.offsetTop - _0x4319db > 0 ? _0x781f01.offsetTop - _0x4319db : 0;
+            let _0x2568bb = _0x781f01.offsetLeft - _0x1e4055 > 0 ? _0x781f01.offsetLeft - _0x1e4055 : 0;
+            _0x781f01.style.top = _0x384879 + 'px';
+            _0x781f01.style.left = _0x2568bb + 'px';
+          };
+        };
+      }
+      ;
+      _0x562c4b();
+    }
+  } catch (_0xbc46b0) {
+    const _0x54df51 = confirm("Script is broken. Do you want to get the updated version?");
+    if (_0x54df51) {
+      return window.open("https://twitch.tv/");
+    }
+  }
+})();
