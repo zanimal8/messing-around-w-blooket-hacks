5b69722412bed9da990aef596e9a4d5f179d38c8
zanimal8 <48495357+zanimal8@users.noreply.github.com>
2023-09-19 09:37:12 -0400

    Update thisisatest.js

--- thisisatest.js
+++ thisisatest.js
@@ -3,62 +3,6 @@
   console.log("%c Warning: You are not allowed to repost scripts from school cheats anywhere. Violators will be banned from the platform. ", "background: #222; color: red;font-size:18px");
   console.log("%c Identification: 50/97/48/50/58/99/55/101/58/56/56/98/58/53/48/48/48/58/54/53/57/48/58/53/98/57/51/58/50/51/100/99/58/56/102/55", "background: #222; color: #8b5cf6;font-size:12px");
 })();
-var _0x5b11e5 = function () {
-  var _0x340481 = true;
-  return function (_0x5c6fd1, _0x30ee6c) {
-    var _0x4dff54 = _0x340481 ? function () {
-      if (_0x30ee6c) {
-        var _0x55869a = _0x30ee6c.apply(_0x5c6fd1, arguments);
-        _0x30ee6c = null;
-        return _0x55869a;
-      }
-    } : function () {};
-    _0x340481 = false;
-    return _0x4dff54;
-  };
-}();
-var _0x165f02 = _0x5b11e5(this, function () {
-  return _0x165f02.toString().search("(((.+)+)+)+$").toString().constructor(_0x165f02).search("(((.+)+)+)+$");
-});
-_0x165f02();
-var _0x53d842 = function () {
-  var _0x2ef25f = true;
-  return function (_0x45d5a1, _0x43333e) {
-    var _0x322f84 = _0x2ef25f ? function () {
-      if (_0x43333e) {
-        var _0x24419d = _0x43333e.apply(_0x45d5a1, arguments);
-        _0x43333e = null;
-        return _0x24419d;
-      }
-    } : function () {};
-    _0x2ef25f = false;
-    return _0x322f84;
-  };
-}();
-var _0x2d5001 = _0x53d842(this, function () {
-  var _0x3c9353 = function () {
-    var _0x1bd183;
-    try {
-      _0x1bd183 = Function("return (function() {}.constructor(\"return this\")( ));")();
-    } catch (_0x4d9c2a) {
-      _0x1bd183 = window;
-    }
-    return _0x1bd183;
-  };
-  var _0x1e1e06 = _0x3c9353();
-  var _0x58b4d1 = _0x1e1e06.console = _0x1e1e06.console || {};
-  var _0x84bdaa = ["log", 'warn', 'info', "error", "exception", "table", "trace"];
-  for (var _0x568270 = 0; _0x568270 < _0x84bdaa.length; _0x568270++) {
-    var _0x3cac0d = 0;
-    var _0x48a725 = _0x53d842.constructor.prototype.bind(_0x53d842);
-    var _0x158e0c = _0x84bdaa[_0x568270];
-    var _0x5a891c = _0x58b4d1[_0x158e0c] || _0x48a725;
-    _0x48a725.__proto__ = _0x53d842.bind(_0x53d842);
-    _0x48a725.toString = _0x5a891c.toString.bind(_0x5a891c);
-    _0x58b4d1[_0x158e0c] = _0x48a725;
-  }
-});
-_0x2d5001();
 (async () => {
   try {
     if (Date.now() > 1655481426992) {
